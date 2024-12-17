49359614d59f8a44c9751f133e4241f4a88834c2
Harish s.s <148946751+gbbgsbyuuysagxbdsajgt@users.noreply.github.com>
2024-12-17 17:25:50 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,26 +5,34 @@ import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
+  // WhatsApp group link
+  const whatsappGroupLink = "https://chat.whatsapp.com/E9IExGsSyZ47UpgSp394FG";
+
+  const handleOrderNowClick = () => {
+    // Open the WhatsApp group link in a new tab
+    window.open(whatsappGroupLink, "_blank");
+  };
+
   return (
     <div className="home-container">
       <Navbar />
       <div className="home-banner-container">
         <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="" />
+          <img src={BannerBackground} alt="Banner Background" />
         </div>
         <div className="home-text-section">
-          <h1 className="primary-heading">
-            SRI SHABARI JEWELLERY 
-          </h1>
+          <h1 className="primary-heading">SRI SHABARI JEWELLERY</h1>
           <p className="primary-text">
-          "At Shabari Jewellery, we take care of everything—designing, crafting, and perfecting every piece—so you can simply adorn yourself with elegance and shine effortlessly."
+            "At Shabari Jewellery, we take care of everything—designing, crafting,
+            and perfecting every piece—so you can simply adorn yourself with elegance
+            and shine effortlessly."
           </p>
-          <button className="secondary-button">
-            Order Now <FiArrowRight />{" "}
+          <button className="secondary-button" onClick={handleOrderNowClick}>
+            Order Now <FiArrowRight />
           </button>
         </div>
         <div className="home-image-section">
-          <img src={BannerImage} alt="" />
+          <img src={BannerImage} alt="Banner" />
         </div>
       </div>
     </div>
